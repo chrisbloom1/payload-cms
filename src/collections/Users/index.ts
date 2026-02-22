eab98da5b5e4096e8cbfb74049d8b8c7428f4445
Rasmus Puls <14924038+rpuls@users.noreply.github.com>
2026-02-22 01:32:13 +0100

    fix usersession

--- src/collections/Users/index.ts
+++ src/collections/Users/index.ts
@@ -15,7 +15,9 @@ export const Users: CollectionConfig = {
     defaultColumns: ['name', 'email'],
     useAsTitle: 'name',
   },
-  auth: true,
+  auth: {
+    useSessions: false,
+  },
   fields: [
     {
       name: 'name',
